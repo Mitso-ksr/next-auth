2134991aa2ef344499dc2d4ae0704e70fed06774
mitso-ksr <mitsonary@gmail.com>
2024-09-26 23:30:16 +0330

    finish up

--- app/page.js
+++ app/page.js
@@ -1,6 +1,12 @@
 import AuthForm from "@/components/auth-form";
+import { verifyAuth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function Home({ searchParams }) {
+  const result = await verifyAuth();
+  if (result.user) {
+    return redirect("/training");
+  }
   const mode = searchParams.mode || "login";
 
   return <AuthForm mode={mode} />;
